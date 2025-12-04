c728ea12bb12acb4ad6fde99ca8c8ccff311be3f
Pintér Gábor <pinter.gabor@gmx.at>
2025-12-04 17:54:44 +0100

    Bump versions.

--- src/main/java/eu/pintergabor/philosophersstone/util/ModUtil.java
+++ src/main/java/eu/pintergabor/philosophersstone/util/ModUtil.java
@@ -10,6 +10,8 @@ import net.minecraft.world.item.alchemy.PotionContents;
 import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.level.ItemLike;
 
+import org.jetbrains.annotations.NotNull;
+
 
 /**
  * Common, unclassified, utilities for the mod.
@@ -30,7 +32,7 @@ public final class ModUtil {
 	 * @return whether the {@code stack} is of {@code item}.
 	 */
 	@SuppressWarnings("unused")
-	public static boolean isItem(ItemStack stack, ItemLike item) {
+	public static boolean isItem(@NotNull ItemStack stack, @NotNull ItemLike item) {
 		return stack.is(item.asItem());
 	}
 
@@ -40,7 +42,7 @@ public final class ModUtil {
 	 * Similar to {@link ItemStack#is(Item)}.
 	 */
 	@SuppressWarnings("unused")
-	public static boolean sameItem(ItemStack stack1, ItemStack stack2) {
+	public static boolean sameItem(@NotNull ItemStack stack1, @NotNull ItemStack stack2) {
 		return stack1.getItem() == stack2.getItem();
 	}
 
@@ -54,7 +56,7 @@ public final class ModUtil {
 	 * @return whether the {@code stack} is of {@code potion}.
 	 */
 	@SuppressWarnings("unused")
-	public static boolean isPotion(ItemStack stack, Holder<Potion> potion) {
+	public static boolean isPotion(@NotNull ItemStack stack, Holder<Potion> potion) {
 		return stack.is(Items.POTION) &&
 			stack.getOrDefault(DataComponents.POTION_CONTENTS, PotionContents.EMPTY)
 				.is(potion);
